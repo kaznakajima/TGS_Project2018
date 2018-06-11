058756e377b8b8a281d1bd3e32456d6a9b17be88
カキハナ_NKC <nkc.taisei.kakihana2016@gmail.com>
2018-06-11 14:59:10 +0900

    バグ修正

--- TGS_Project2018/Assets/kakihana_Data/Scripts_ByKakihana/Player.cs
+++ TGS_Project2018/Assets/kakihana_Data/Scripts_ByKakihana/Player.cs
@@ -213,7 +213,8 @@ public class Player : StatusController {
             }
         }else if(status == _status && pageChange.pageFlip < -1)
         {
-              FormChange((int)ANIM_ENUMS.BLUCK.IDLE, _status);
+
+              FormChange((int)ANIM_ENUMS.BLUCK.IDLE, STATUS.NONE);
         }
     }
 
